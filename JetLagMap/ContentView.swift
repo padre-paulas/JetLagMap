dded4608eed352c95a9b9dcaf26b2ed2737957bb
Pavlo Prokhorov <pavlo.nord@gmail.com>
2025-04-18 17:23:28 +0300

    Initial Commit

---
+++ JetLagMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JetLagMap
+//
+//  Created by Pavlo Prokhorov on 18.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JetLagMap/JetLagMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  JetLagMapApp.swift
+//  JetLagMap
+//
+//  Created by Pavlo Prokhorov on 18.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct JetLagMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
